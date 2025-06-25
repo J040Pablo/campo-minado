69e8633dc5c5ff90d7966723d2d86b47ff6d7ae3
J040Pablo <aguadetorneira7@gmail.com>
2025-06-25 20:12:51 -0300

    Primeiro commit do projeto Campo Minado

---
+++ test/br/com/cod3r/cm/modelo/CampoTeste.java
@@ -0,0 +1,57 @@
+
+package br.com.cod3r.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3,3);
+	}
+	@Test
+	void testeVizinhoRealDistancialEsquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancialDireita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancialEmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancialEmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoRealDistancial2() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+}
